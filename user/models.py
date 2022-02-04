e3635e690689843d46203f33db1b92c3281722d0
hyerim Kim <hyerim980513@gmail.com>
2022-02-04 21:30:09 +0900

    [FEAT] add models and initial settings

--- user/models.py
+++ user/models.py
@@ -11,8 +11,11 @@ class User(AbstractUser):
     email = models.CharField(verbose_name="이메일",max_length=50)
     nickName = models.CharField(verbose_name="닉네임", max_length=30)
     gender = models.CharField(verbose_name="성별", max_length=10, choices=GENDER_CHOICES)
-    age = models.IntegerField(verbose_name="나이")
-    imageUrl = models.ImageField(upload_to="user_image", null=True, blank=True)
+    age = models.IntegerField(verbose_name="나이", null=True)
+    imageUrl = models.ImageField(upload_to="user_img", null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
     
 class Music(models.Model):
     artist = models.CharField(verbose_name="가수", max_length=20)
@@ -20,16 +23,25 @@ class Music(models.Model):
     albumTitle = models.CharField(verbose_name="앨범 이름", max_length=50)
     releasedDate = models.DateField(verbose_name="출시일")
     lyric = models.TextField(verbose_name="가사")
-    albumCoverUrl = models.ImageField(upload_to="album_image", null=True, blank=True)    
+    albumCoverUrl = models.ImageField(upload_to="album_img", null=True, blank=True)
+    
+    def __str__(self):
+        return self.title
 
 
 class Certification(models.Model):
     createdDate = models.DateField(verbose_name="인증 날짜")
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user", verbose_name="유저 id")
-    music_id = models.ForeignKey(Music, on_delete=models.CASCADE, related_name="music", verbose_name="music id")
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="certification_userID", verbose_name="유저 id")
+    music_id = models.ForeignKey(Music, on_delete=models.CASCADE, related_name="certification_musicID", verbose_name="music id")
+    
+    def __str__(self):
+        return self.createdDate
     
 class Community(models.Model):
     communityName = models.CharField(verbose_name="커뮤니티 이름", max_length=50)
     createdDate = models.DateTimeField(verbose_name="커뮤니티 생성일")
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user", verbose_name="유저 id")
-    music_id = models.ForeignKey(Music, on_delete=models.CASCADE, related_name="music", verbose_name="music id")
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Community_userID", verbose_name="유저 id")
+    music_id = models.ForeignKey(Music, on_delete=models.CASCADE, related_name="Community_musicID", verbose_name="music id")
+    
+    def __str__(self):
+        return self.communityName
