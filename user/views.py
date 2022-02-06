0fa9313b02794e67d18daaefa5ed80ebd29fd201
Gaeun Kim <gaeun0204@gmail.com>
2022-02-07 05:17:26 +0900

    [FEAT] Init musicDetail

--- user/views.py
+++ user/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
+def musicDetail(request, pk):
+	music = Music.objects.get(id=pk)
+	ctx = {'music':music}
+	return render(request, template_name = 'musicDetail.html', context = ctx)
+
+# def musicDelete(request, pk):
+#     music = get_object_or_404(Music, id=pk)
+#     music.delete()
+# 	return
+# 	#return redirect('user:myPage')
