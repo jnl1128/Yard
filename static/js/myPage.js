b02548de9f08b6064ca1002433c789e1ffa2609b
narin <nalynnshinhwa1128@gmail.com>
2022-02-10 15:24:01 +0900

    [REFACT] 인증서 슬라이드 기능

--- static/js/myPage.js
+++ static/js/myPage.js
@@ -1,7 +1,9 @@
 const certificates = document.querySelectorAll('.certificate-container');
+const certificateList = document.querySelector('.certificate-list')
+const moreCertBtn = document.getElementById('moreCertBtn')
+
 
 function displayMusicInfo(event){
-  console.log(event.target.childNodes[3])
   event.target.childNodes[3].classList.toggle('hidden');
   event.target.childNodes[1].style.filter = 'brightness(50%)'
 }
@@ -9,6 +11,17 @@ function hideMusicInfo(event){
   event.target.childNodes[3].classList.add('hidden')
   event.target.childNodes[1].style.filter = 'brightness(100%)'
 }
+
+function slideRight(){
+  console.log('click')
+  const slideX = 170 * -1
+  
+  certificateList.style.transform = `translateX(${slideX}px)`
+  certificateList.style.overflow = 'visible'
+  //certificateList.style.transfrom = `translateX(${-100}px)`
+}
+
+//eventListener
 [].forEach.call(certificates, function(certificate){
   certificate.addEventListener('mouseenter', displayMusicInfo)
 
@@ -17,3 +30,6 @@ function hideMusicInfo(event){
   certificate.addEventListener('mouseleave', hideMusicInfo);
 
 });
+
+
+moreCertBtn.addEventListener('click', slideRight);
