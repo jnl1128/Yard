fdb61f09b3008845338e27ec5e5513d7f93717df
paulyoon28 <ekdmbzzang@naver.com>
2022-02-11 11:24:25 +0900

    [REFACT]replace username to email

--- config/settings.py
+++ config/settings.py
@@ -150,4 +150,16 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+
+ACCOUNT_EMAIL_VERIFICATION = 'optional'
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    "django.contrib.auth.backends.ModelBackend",
+    # `allauth` specific authentication methods, such as login by e-mail
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
