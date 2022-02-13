b6e69345a0c1f23b9aabe0c483370f20831717af
hyerim Kim <hyerim980513@gmail.com>
2022-02-14 00:45:41 +0900

    [FEAT] 음악 검색기능, 중복 체크 미허용, input enter redirect 오류 해결

--- user/urls.py
+++ user/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
   path('register/musicsearch/', views.musicSearch, name='musicSearch'),
   path('search/',views.searchResult, name='search'),
   path('createFeed/', views.createFeed, name="createFeed"),
+  path('createFeed/addMusicAjax/', views.addMusicAjax, name='addMusicAjax'),
   path('feed/<int:pk>/', views.feedDetail, name="feedDetail"),
   path('mypage/<int:pk>/', views.certDetail, name='certDetail'),
-  path('add_comment_ajax/', views.add_comment_ajax, name='add_comment_ajax'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
