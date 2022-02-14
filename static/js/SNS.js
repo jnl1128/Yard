f8c44ba8b23c08bebe39d4e92ed3bf78db2c904d
narin <nalynnshinhwa1128@gmail.com>
2022-02-14 19:07:06 +0900

    [FEAT] sns로 링크 공유

---
+++ static/js/SNS.js
@@ -0,0 +1,11 @@
+function shareTwitter() {
+  var sendText = "개발새발"; // 전달할 텍스트
+  var sendUrl = "http://127.0.0.1:5500/"; // 전달할 URL
+  window.open("https://twitter.com/intent/tweet?text=" + sendText + "&url=" + sendUrl);
+}
+
+function shareFacebook() {
+  var sendUrl = "http://127.0.0.1:5500/"; // 전달할 URL
+  window.open("http://www.facebook.com/sharer/sharer.php?u=" + sendUrl);
+}
+
