7c28d89c31928692588aee6083986f31fbf11af8
Gaeun Kim <gaeun0204@gmail.com>
2022-02-20 16:46:31 +0900

    [REFECT] certDetail CSS 수정 (미완)

--- collectstatic/js/myPage.js
+++ collectstatic/js/myPage.js
@@ -4,11 +4,10 @@ const certificate_rightBtn = document.getElementById('certificate-rightBtn')
 const certificate_leftBtn = document.getElementById('certificate-leftBtn')
 
 const certificate_btn = document.getElementById('certificate-btn')
-
+console.log(certificates.length)
 function displayMusicInfo(event){
   event.target.childNodes[3].classList.toggle('hidden');
   event.target.childNodes[1].style.filter = 'brightness(50%)'
-  
 }
 function hideMusicInfo(event){
   event.target.childNodes[3].classList.add('hidden')
@@ -30,30 +29,33 @@ function hideMusicInfo(event){
 
 function slideCert(event){
   let direction = 0;
-  const certificateLen = certificates.length
-  certificates.forEach(function(certificate){
-    let temp = Number(certificate.style.left.slice(0,4))
-    if (isNaN(temp)) temp = 0
-
-    if(event.target === certificate_rightBtn){
-      direction = -170;
-      if((temp+direction) < (certificateLen) * direction){
-        return 
+  const certificateLen = certificates.length -1 
+  if(certificateLen > 1){
+    certificates.forEach(function(certificate){
+      let temp = Number(certificate.style.left.slice(0,4))
+      if (isNaN(temp)) temp = 0
+  
+      if(event.target === certificate_rightBtn){
+        direction = -170;
+        if((temp+direction) < (certificateLen) * direction){
+          return 
+        }
       }
-    }
-    else if(event.target === certificate_leftBtn){
-      direction = 170;
-      if(temp == 0  || isNaN(temp)){
-        return 
+      else if(event.target === certificate_leftBtn){
+        direction = 170;
+        if(temp == 0  || isNaN(temp)){
+          return 
+        }
       }
-    }
-    else{
-      return
-    }
-
-    const newLeft  = String(temp + direction)
-    certificate.style.left = newLeft + 'px'
-  })
+      else{
+        return
+      }
+  
+      const newLeft  = String(temp + direction)
+      certificate.style.left = newLeft + 'px'
+    })
+  }
+  
   
 }
 
