59c7848ba63cbb463a87a7166facbec30b99ddbd
narin <nalynnshinhwa1128@gmail.com>
2022-02-20 22:04:29 +0900

    [REFAACT] feedpage layout 수정

---
+++ static/js/feedpage.js
@@ -0,0 +1,13 @@
+const tagBtn = document.getElementById("tag-list-btn")
+const tagList = document.getElementById('tag-list')
+
+const feedContainer = document.getElementById('feed-list')
+
+console.log(tagList)
+function toggleTagList(){
+  tagBtn.classList.toggle('open')
+  tagList.classList.toggle('tagClose')
+  //feedContainer.classList.toggle('small')
+}
+
+tagBtn.addEventListener('click', toggleTagList)
