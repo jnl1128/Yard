99fdd1d7dc3deb790173383877866fa947abe03b
narin <nalynnshinhwa1128@gmail.com>
2022-02-22 05:00:34 +0900

    [STYLE] 폰트 수정 전

--- static/js/feed1.js
+++ static/js/feed1.js
@@ -110,5 +110,5 @@ function autolink() {
     var regURL = new RegExp("(http|https|ftp|telnet|news|irc)://([-/.a-zA-Z0-9_~#%$?&=:200-377()]+)","gi");
     container[i].innerHTML = doc.replace(regURL,"<a href='$1://$2' class='text-decoration-underline text-primary' target='_blank'>$1://$2</a>");
   }
-
-  autolink();
+}
+autolink();
