1382954050ddea0603c618078f4c2c563db89f3b
narin <nalynnshinhwa1128@gmail.com>
2022-02-21 21:37:06 +0900

    [STYLE] feed create, update form 수정

--- static/js/feedpage.js
+++ static/js/feedpage.js
@@ -3,7 +3,6 @@ const tagList = document.getElementById('tag-list')
 
 const feedContainer = document.getElementById('feed-list')
 
-console.log(tagList)
 function toggleTagList(){
   tagBtn.classList.toggle('open')
   tagList.classList.toggle('tagClose')
@@ -11,3 +10,46 @@ function toggleTagList(){
 }
 
 tagBtn.addEventListener('click', toggleTagList)
+
+const feedForm = document.getElementById('feedForm')
+const feedBody = feedForm.querySelector('.modal-body')
+feedBody.style.display = 'flex'
+feedBody.style.flexDirection='column'
+const feedFormB = document.querySelector('.post-form')
+const musicFeed = document.querySelector('.searchDiv')
+musicFeed[0].classList.add('form-white-btn')
+
+const musicTitle = feedFormB.childNodes[3]
+const musicArtist = feedFormB.childNodes[5]
+const musicTitleInput = musicTitle.querySelector('input')
+const musicArtistInput = musicArtist.querySelector('input')
+musicTitleInput.classList.add('form-white-btn')
+musicTitleInput.classList.add('musicTitleInput')
+musicArtistInput.classList.add('form-white-btn')
+musicArtistInput.classList.add('musicArtistInput')
+
+const ptag = feedFormB.querySelectorAll('p')
+const tagSelection = ptag[2].querySelector('select')
+tagSelection.style.width = '100%';
+tagSelection.style.borderRadius = '0.5rem';
+tagSelection.style.border = '0.1rem solid black'
+tagSelection.style.padding = '0.5rem'
+tagSelection.style.boxShadow = '0px 4px 4px 0px rgba(0, 0, 0, 0.25)';
+const tagOptions = ptag[2].querySelectorAll('option')
+tagOptions.forEach((option)=>{
+  option.style.fontSize = '1.2rem'
+})
+ptag[3].style.display= 'flex'
+ptag[3].style.flexDirection = 'column'
+ptag[4].style.display= 'flex'
+ptag[4].style.flexDirection = 'column'
+
+const hashTagInput = ptag[3].querySelector('input')
+//hashTagInput.classList.add('form-white-btn')
+hashTagInput.style.width = '100%'
+
+const textarea = document.querySelector('textarea')
+textarea.style.borderRadius = '0.5rem';
+textarea.style.border = '0.1rem solid black'
+textarea.style.padding = '0.5rem'
+textarea.style.boxShadow = '0px 4px 4px 0px rgba(0, 0, 0, 0.25)';
